59cf86945a8e719b1321bea955b8bd9c29baafde
Ferdinando Traversa <ferdi.traversa@gmail.com>
2017-04-17 13:00:03 +0200

    Update BuildVars.java

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -13,12 +13,12 @@ public class BuildVars {
     public static boolean DEBUG_PRIVATE_VERSION = false;
     public static int BUILD_VERSION = 2;
     public static String BUILD_VERSION_STRING = "0.8";
-    public static int APP_ID = 82818; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
-    public static String APP_HASH = "caecb55a559ed638c435bad6aa9eb048"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
-    public static String HOCKEY_APP_HASH = "c987c3620dd441fab3d18247cc1c746c";
-    public static String HOCKEY_APP_HASH_DEBUG = "2e9ab8d09ebd9b0e25108c7975379de8";
-    public static String BING_SEARCH_KEY = "13fdfb40ef5b42088a1e5866553317aa"; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
-    public static String FOURSQUARE_API_KEY = "X3P22SYOTG3EOT1Y3P11LUOQPJY2HUDP0WWKL4200HD10J55"; //obtain your own KEY at https://developer.foursquare.com/
-    public static String FOURSQUARE_API_ID = "YUCLGYAJSL5JXCVSIRVY3YJQOX1431FTG0MF2H01DTLDUEPM"; //obtain your own API_ID at https://developer.foursquare.com/
-    public static String FOURSQUARE_API_VERSION = "20150326";
+    public static int APP_ID = 1; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+    public static String APP_HASH = ""; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static String HOCKEY_APP_HASH = "";
+    public static String HOCKEY_APP_HASH_DEBUG = "";
+    public static String BING_SEARCH_KEY = ""; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
+    public static String FOURSQUARE_API_KEY = ""; //obtain your own KEY at https://developer.foursquare.com/
+    public static String FOURSQUARE_API_ID = ""; //obtain your own API_ID at https://developer.foursquare.com/
+    public static String FOURSQUARE_API_VERSION = "";
 }
